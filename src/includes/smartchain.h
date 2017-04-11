399bc81b1458f37b00baffb100d837b4ccac7c61
nschaetti <n.schaetti@gmail.com>
2017-04-11 17:12:44 +0200

    Initial commit

---
+++ src/includes/smartchain.h
@@ -0,0 +1,8 @@
+#ifndef __SMARTCOIN_H__
+#define __SMARTCOIN_H__
+
+#include <stdio.h>
+
+void test_fn(void);
+
+#endif
---
+++ src/libsmartchain.cc
@@ -0,0 +1,9 @@
+#include "includes/smartchain.h"
+
+// Gets rid of annoying "deprecated conversion from string constant blah blah"
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+void test_fn(void)
+{
+	printf("Hello SmartChain!!\n");
+}
